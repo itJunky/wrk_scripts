104e06688667dc7d52933be8b796e998a820711a
Max <alphaQu4z4r@gmail.com>
2013-08-05 21:42:05 +0400

    Change pathes

--- local_backup.sh
+++ local_backup.sh
@@ -1,38 +1,40 @@
 #!/usr/local/bin/bash
 # Backup script for BSD web-servers by itJunkie
 
-LHOST="ara"                     # Local Host
-RHOST="lucky.example.com"       # Rmote Host
+LHOST="arahost"                 # Local Host
+RHOST="bkp.example.com"         # Rmote Host
 DATE=`date +%Y-%m-%d`           # Current date
+BACKUP_DIR="/backup"            # Directory for backups
+PATH_TO_BACKUP=/${BACKUP_DIR}/${LHOST}
 
-function create_bac()
+function create_backup()
 {       # Creating files backup
         FILENAME="${DATE}-files.tar.gz"
-        tar -czf /usr/backup/${LHOST}/${FILENAME} /www/*
-        # Executing MySQL backup function
-        mysql_bac 
+        tar -czf /${PATH_TO_BACKUP}/${FILENAME} /www/*
+        
+        ### Executing MySQL backup function ###
+        mysql_backup
 }
 
-function mysql_bac()
-{       # MySQL backup
-        mkdir /usr/backup/${LHOST}/db   # make temporary dir
+function mysql_backup()
+{       # Creating MySQL databases backup 
+        mkdir /${PATH_TO_BACKUP}/db     # make temporary dir
         FILENAME="${DATE}-db.tar.gz"    # Name of archive file
 
         for dtb in `mysqlshow | grep -vE 'information_schema|Databases|----' | awk '{print $2}'`
         do      # Dump databas by turn
                 echo "Dumping $dtb"
-                mysqldump ${dtb} > /usr/backup/${LHOST}/db/${dtb}.sql
+                mysqldump ${dtb} > /${PATH_TO_BACKUP}/db/${dtb}.sql
         done
 
-        #ls -l /usr/backup/${LHOST}/db/ # DEBUG
-        tar -czf /usr/backup/${LHOST}/${FILENAME} /usr/backup/${LHOST}/db
+        #ls -l /${PATH_TO_BACKUP}/db/   # DEBUG
+        tar -czf /${PATH_TO_BACKUP}/${FILENAME} /${PATH_TO_BACKUP}/db
         echo "Removing temporary files"
-        rm /usr/backup/${LHOST}/db/*.sql
+        rm /${PATH_TO_BACKUP}/db/*.sql
 }
 
-function sync_bac()
+function sync_backup()
 {       # copying backup to remote server
-        PATH_TO_BACKUP=/usr/backup/${LHOST}
         LATEST_FILES=`find $PATH_TO_BACKUP -type f -name "*" -print0 | xargs -0 ls -t`
         ARRAY_FILES=($LATEST_FILES)
         #echo ${ARRAY_FILES[*]}         # DEBUG
@@ -42,6 +44,6 @@ function sync_bac()
 
 ### MAIN ###
 
-create_bac
-#mysql_bac
-sync_bac
+create_backup
+#mysql_backup  # Exec from create_backup()
+sync_backup
